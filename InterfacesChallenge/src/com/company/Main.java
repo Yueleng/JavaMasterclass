196ee10206e19bafe271b31fdf6d179af8716888
Alan Wang <wangyueleng@gmail.com>
2018-01-25 00:07:38 -0500

    __
    12:07
    1/25/2018

--- InterfacesChallenge/src/com/company/Main.java
+++ InterfacesChallenge/src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,6 +11,75 @@ public class Main {
         // The interface will just specify 2 methods, one to return an ArrayList of values to be saved
         // and the other to populate the object's field from an ArrayList.
         //
-        // Create some s
+        // Create some sample classes that implement your saveable interface (We've used the idea of a game,
+        // with Players and Monsters, but you can create any type of classes that you want).
+        //
+        // Override the toString() method for each of your classes so that they can be easily printed to enable the program to be tested easier.
+        //
+        // In Main, write a method that takes an object that implements the interface as a parameter and "saves" the values.
+        // We haven't covered I/O yet, so your method should just print the values to the screen.
+        // Also in Main, write a method that restores the values to a saveable object.
+        // Again, we are not going to use Java file I/O; instead use the readValues() method below to simulate getting values from a file - this allows you to type
+        // as many values as your class requires, and returns an ArrayList.
+        Player tim = new Player("Tim", 10,15);
+        System.out.println(tim.toString()); // Player{name='Tim', hitPoints=10, strength=15, weapon='Sword'}
+        System.out.println("*******************");
+        saveObject(tim);
+        System.out.println("*******************");
+        tim.setHitPoints(8);
+        System.out.println(tim); // call toString() by default; Player{name='Tim', hitPoints=10, strength=15, weapon='Sword'}
+        System.out.println("*******************");
+        tim.setWeapon("Stormbringer");
+        saveObject(tim);
+        System.out.println("*******************");
+        loadObject(tim);
+        System.out.println(tim);
+
+        ISaveable warewolf = new Monster("Warewolf", 20,40);
+        System.out.println(warewolf);
+        saveObject(warewolf);
+
+
+    }
+
+    public static ArrayList<String> readValues() {
+        ArrayList<String> values = new ArrayList<String>();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose\n" +
+                            "1 to enter a string\n" +
+                            "0 to quit");
+
+        while (!quit) {
+            System.out.println("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.println("Enter a string: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
+    }
+
+    public static void saveObject(ISaveable objectTosave) {
+        for (int i =0; i < objectTosave.write().size(); i++) {
+            System.out.println("Saving " + objectTosave.write().get(i) + " to storage device");
+        }
+    }
+
+    public static void loadObject(ISaveable objectToLoad) {
+        ArrayList<String> values = readValues();
+        objectToLoad.read(values);
+
     }
 }
